f8b6b971ba328477930819aa56713242c2b7a067
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-11 01:32:49 +0300

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Max delay module """
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ spawn wait_random n times and return sorted list of values"""
+
+    list = []
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        list.append(delay)
+    return list
