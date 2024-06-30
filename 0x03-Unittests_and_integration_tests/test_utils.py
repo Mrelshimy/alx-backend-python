fb5c17026235ba9917c73db29b68a5b2d288b9df
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-30 15:06:19 +0300

    task2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ utils.py test module """
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ utils.py test class """
+    """ utils.py access_nested_map test class """
 
     @parameterized.expand([
         ({"a": {"b": {"c": 1}}}, "abc", 1),
@@ -25,3 +26,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test_access_nested_map_exception test method """
         with self.assertRaises(expected):
             access_nested_map(input, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ utils.py get_json test class """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, expected, mock_get):
+        """ test get json method """
+
+        mock_get.return_value.json.return_value = expected
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, expected)
