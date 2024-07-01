b4dcd5b124ad0f9b1ccc067d3a7ccfa922028ba1
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-01 11:51:41 +0300

    task6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -39,3 +39,13 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch.object(GithubOrgClient, '_public_repos_url',
                           new_callable=PropertyMock) as org_mock:
             org_mock.return_value = 'abc'
+
+    @parameterized.expand([
+        ({'license': {"key": "my_license"}}, "my_license", True),
+        ({'license': {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ test has_license method """
+        obj = GithubOrgClient('abc')
+        result = obj.has_license(repo, license_key)
+        self.assertEqual(result, expected)
