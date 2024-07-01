bea832fdfdc314d6d18c05d91dbc9ba33499d8d2
Mrelshimy <mraafat.elsayed@gmail.com>
2024-07-01 11:32:31 +0300

    task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,3 +30,13 @@ class TestGithubOrgClient(unittest.TestCase):
             org_mock.return_value = 'abc'
             obj = GithubOrgClient('abc')
             self.assertEqual(obj._public_repos_url, 'abc')
+
+    @patch('client.get_json')
+    def test_public_repos(self, url, mock_get):
+        """ test_public repos method """
+        return_value = {'a': 'b'}
+        mock_get.return_value = return_value
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as org_mock:
+            org_mock.return_value = 'abc'
+            
